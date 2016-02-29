1e0492a313af9e10f0e6087f8bc522e3f9be4c1d
sumayahalmohanna <sumayahsaur@gmail.com>
2016-02-29 14:18:45 +0300

    Update
    Added to top button.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
 	$("#more").click(function(e) {
         e.preventDefault();
     });
+	
+	var offset = 300, offsetOpacity = 1200, scrollTopDuration = 700, $back_to_top = $('.top');
+	
+	$(window).scroll(function() {
+		
+		($(this).scrollTop() > offset) ? $back_to_top.addClass('top-is-visible') : $back_to_top.removeClass('top-is-visible top-fade-out');
+		
+		if($(this).scrollTop() > offsetOpacity)
+		{
+			$back_to_top.addClass('top-fade-out');
+		}
+	});
+	
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scrollTopDuration
+		);
+	});
+	
 });
